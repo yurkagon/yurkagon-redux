a02f2c9b277fc0b101efaa363b231c3fbb8f05b6
yurkagon <owngameplay@gmail.com>
2018-09-22 17:02:36 +0300

    [UPD] Logger

--- src/helpers/redux-logger.js
+++ src/helpers/redux-logger.js
@@ -1,10 +1,12 @@
 const logger = (newState, prevState, action) => {
+  console.group();
   console.log({prevState});
   console.log({
     action: action.type,
     payload: action.payload,
   });
   console.log({newState});
+  console.groupEnd();
 };
 
 export default logger;
